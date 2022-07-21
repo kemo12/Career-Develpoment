e27382d2b62d470fe9def6aa11aec71ef9bbda1c
kemo12 <dodkamel919@gmail.com>
2022-07-21 12:16:30 +0300

    iniaite files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import SignUp from './pages/SignUp';
+import Home from './pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <Home />
     </div>
   );
 }
